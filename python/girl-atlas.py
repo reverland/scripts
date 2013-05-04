dac99cf07ef4320e391dc52f06f13a4b7b1f453c
Yuyang Liu <lhtlyy@gmail.com>
2013-05-04 21:06:59 +0800

    modified:   python/girl-atlas.py

--- python/girl-atlas.py
+++ python/girl-atlas.py
@@ -5,39 +5,36 @@
 A little script to download images from 'http://girl-atlas.com/'
 Written for fun by Reverland(lhtlyy@gmail.com)
 Free to use and with no warranty, BSD License.
-
-试了试多线程结果不会限制连接数……
-
-结果就是……
-
-HTTPConnectionPool(host='127.0.0.1', port=1998): Max retries exceeded with url: http://girlatlas.b0.upaiyun.com/97/20130124/00539b1f575b3b305542.jpg (Caused by <class 'httplib.BadStatusLine'>: '')
-
 """
 
-# import urllib
+from gevent import monkey
+monkey.patch_all()
+import requests
+#import urllib
 import os
-import urllib2
+#import urllib2
 import re
 import argparse
 parser = argparse.ArgumentParser()
-from gevent import monkey
-monkey.patch_all()
+#import gevent
 
 
-def http_proxy_handlers(proxy):
-    """dirty work, change opener"""
-    proxy_handler = urllib2.ProxyHandler({"http": proxy})
-    opener = urllib2.build_opener(proxy_handler)
-    urllib2.install_opener(opener)
+# def http_proxy_handlers(proxy):
+#     """dirty work, change opener"""
+#     proxy_handler = urllib2.ProxyHandler({"http": proxy})
+#     opener = urllib2.build_opener(proxy_handler)
+#     urllib2.install_opener(opener)
 
 
-def get_html(url):
+def get_html(url, proxy=None):
     """get html"""
-    req = urllib2.Request(url)
-    res = urllib2.urlopen(req)
+    #req = urllib2.Request(url)
+    #res = urllib2.urlopen(req)
     # print "pull html file"
-    html = res.read()
+    #html = res.read()
     # print "end of pulling"
+    r = requests.get(url, proxies=proxy)
+    html = r.text
     return html
 
 
@@ -51,14 +48,14 @@ def download(result, path):
     """dirty works, download all files in (name, url) list"""
     for name, url in result:
         print ">>> Begin download %s..." % name
-        # urllib.urlretrieve(url, name + '.jpg')
-        # print path + name
-        if os.path.exists(path + name):
+        if os.path.exists(path + name + '.jpeg'):
+            print "%s exists, skip..." % name
             continue
-            print "%s exists" % name
         try:
-            with open(path + name, 'wb') as f:
-                f.write(urllib2.urlopen(url).read())
+            r = requests.get(url)
+            with open(path + name + '.jpeg', 'wb') as f:
+                f.write(r.content)
+            #urllib.urlretrieve(url, path + name + '.jpeg')
             print ">>> %s downloaded" % name
         except Exception, e:
             print "%s" % e
@@ -67,29 +64,32 @@ def download(result, path):
 
 def download_by_url(url, path='./'):
     pattern = re.compile('<img title="([^"]+)" src=\'([^!]+)!mid\' />')
-    result = parse_image_urls(get_html(url), pattern)
+    result = parse_image_urls(get_html(url, proxy), pattern)
     download(result, path)
 
 
 def download_by_tags(url):
     """download by tags"""
-    html = get_html(url)
+    html = get_html(url, proxy)
     next_page = re.compile("<a class=\"btn-form next\" href='([^']+)'>Next</a>").search(html)
     # print "next", next
     while next_page:
         url = 'http://girl-atlas.com' + next_page.group(1)
         # print url
-        html_next = get_html(url)
+        html_next = get_html(url, proxy)
         html = html + html_next
         next_page = re.compile("<a class=\"btn-form next\" href='([^']+)'>Next</a>").search(html_next)
     pattern = re.compile("<a href='([^']+)' class=\"caption\">(.+)</a>")
     result = parse_image_urls(html, pattern)
+    # good enough with gevent, however I comment it.
+    # jobs = [gevent.spawn(download_by_url, url, name + '/') for (url, name) in result]
     for album_url, album_name in result:
         album_name = album_name.replace('/', '')
         try:
             os.mkdir(album_name)
         except:
             pass
+    # gevent.joinall(jobs)
         download_by_url(album_url, album_name + '/')
 
 # download_by_tags('http://girl-atlas.com/t/40')
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # print args
     if args.p:
-        http_proxy_handlers(args.p)
+        proxy = {'http': args.p}
     if args.a:
         print ">>>Downloading from %s now" % args.a
         download_by_url(args.a)
